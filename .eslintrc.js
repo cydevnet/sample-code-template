4797b3cf03a589743f81417a433fe9cd56c741bc
piyo <chick.p.1120@gmail.com>
2022-02-16 11:34:02 +0900

    extract settings to each file

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,19 +3,10 @@ module.exports = {
   // 呼び出したいルール（パッケージ）
   // --------------------------------
   // ES5 & kintone の場合
-  extends: "@cybozu/eslint-config/presets/kintone-customize-es5",
+  // extends: ["./es5.js"],
 
   // ES6以上 & kintone の場合
-  // extends: [
-  //   '@cybozu',
-  //   '@cybozu/eslint-config/globals/kintone',
-  // ],
-  // parserOptions: {
-  //   sourceType: 'script',
-  // },
-  // rules: {
-  //   strict: ['error', 'function'],
-  // },
+  // extends: ["./es6.js"],
 
   // node & kintone の場合
   // extends: ["@cybozu/eslint-config/presets/node", "@cybozu/eslint-config/globals/kintone"],
